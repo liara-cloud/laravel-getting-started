9b93a713532689b66caa429f62969451a405852f
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-06-24 15:56:07 +0330

    add voyager --> /admin page

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        TCG\Voyager\VoyagerServiceProvider::class,
+        TCG\Voyager\Providers\VoyagerDummyServiceProvider::class,
 
         /*
          * Package Service Providers...
